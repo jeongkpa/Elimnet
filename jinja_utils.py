77f65e64f4f6f119db3e3a6d744d1257a1c2df47
jeongkpa <134479589+jeongkpa@users.noreply.github.com>
2025-01-13 16:54:53 +0900

    Update jinja_utils.py

--- jinja_utils.py
+++ jinja_utils.py
@@ -1,7 +1,7 @@
 from jinja2 import Environment, FileSystemLoader, Template
 import os
 
-def convert_text_to_html(text: str) -> str:
+def convert_text_to_html(text: str, title: str = "") -> str:
     """일반 텍스트를 HTML 형식으로 변환합니다."""
     # 기본 스타일 정의
     base_style = 'style="font-size: 10pt; font-family: 맑은 고딕; font-style: normal; font-variant-ligatures: normal; ' \
@@ -17,11 +17,16 @@ def convert_text_to_html(text: str) -> str:
                  'white-space: normal; text-decoration-thickness: initial; text-decoration-style: initial; ' \
                  'text-decoration-color: initial; color: #0033CC"'
     
+    # 제목 처리
+    result = []
+    if title:
+        result.append(f'<span {title_style}>{title}</span><br>\n<br>\n')
+    
     # 줄바꿈을 <br>과 들여쓰기로 변환
     paragraphs = text.split('\n\n')  # 빈 줄로 문단 구분
     
     formatted_paragraphs = []
-    for i, p in enumerate(paragraphs):
+    for p in paragraphs:
         # 각 줄을 <br>로 연결
         lines = p.split('\n')
         formatted_lines = []
@@ -31,21 +36,8 @@ def convert_text_to_html(text: str) -> str:
                 if line.startswith(' '):
                     line = '&nbsp;' * (len(line) - len(line.lstrip())) + line.lstrip()
                 
-                # 첫 번째 문단의 첫 줄은 제목 스타일 적용
-                if i == 0 and not formatted_lines:
-                    # 제이씨현시스템㈜ 부분과 나머지 부분 분리
-                    if "제이씨현시스템" in line:
-                        company_part = line[:line.find("제이씨현시스템") + len("제이씨현시스템㈜")]
-                        rest_part = line[line.find("제이씨현시스템") + len("제이씨현시스템㈜"):]
-                        formatted_lines.append(
-                            f'<font size="2" color="#0033CC" style="font-family: 맑은 고딕;"><b>{company_part}</b></font>'
-                            f'<span {title_style}>{rest_part}</span><br>\n'
-                        )
-                    else:
-                        formatted_lines.append(f'<span {title_style}>{line}</span><br>\n')
-                else:
-                    # 나머지 텍스트는 기본 스타일 적용
-                    formatted_lines.append(f'<span lang="EN-US" {base_style}>{line}</span><br>\n')
+                # 모든 텍스트에 기본 스타일 적용
+                formatted_lines.append(f'<span lang="EN-US" {base_style}>{line}</span><br>\n')
         
         # 문단을 합치기
         if formatted_lines:
@@ -55,11 +47,12 @@ def convert_text_to_html(text: str) -> str:
             formatted_paragraphs.append('<br>\n')
     
     # 모든 문단을 합치고 마지막 빈 줄 제거
-    result = ''.join(formatted_paragraphs)
-    if result.endswith('<br>\n'):
-        result = result[:-5]
+    result.extend(formatted_paragraphs)
+    final_result = ''.join(result)
+    if final_result.endswith('<br>\n'):
+        final_result = final_result[:-5]
     
-    return result
+    return final_result
 
 def generate_press_release_html(title: str, body_text: str) -> str:
     """
@@ -76,8 +69,8 @@ def generate_press_release_html(title: str, body_text: str) -> str:
     # press_release_template.html 읽기
     template = env.get_template('press_release_template.html')
 
-    # 일반 텍스트를 HTML로 변환
-    body_html = convert_text_to_html(body_text)
+    # 일반 텍스트를 HTML로 변환 (title 포함)
+    body_html = convert_text_to_html(body_text, title)
     
     # 템플릿 렌더링
     rendered_html = template.render(
